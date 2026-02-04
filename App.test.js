472bb6e8f3e48c81af53befbe8da3e8ccb76161f
Oladayo Aremu <d1arems@gmail.com>
2026-02-04 11:19:26 +0100

    Add files via upload
    DevOps Micro Internship Week2 Projects Completed

--- App.test.js
+++ App.test.js
@@ -1,14 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from '../App';
+import App from './App';
 
-test('renders welcome message', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const heading = screen.getByText(/Welcome to My React App/i);
-  expect(heading).toBeInTheDocument();
-});
-
-test('renders nginx message', () => {
-  render(<App />);
-  const paragraph = screen.getByText(/This app is running on Nginx!/i);
-  expect(paragraph).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
